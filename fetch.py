f65c7f3eb0193aafbf13982b88ceef84d8a4477e
Karolina Alexiou <karolina.alexiou@teralytics.ch>
2014-03-26 18:10:58 +0100

    Added backfilling capabilities, isolated some actions in functions

--- fetch.py
+++ fetch.py
@@ -7,30 +7,71 @@ import time
 import os 
 import numpy as np
 
+def datetime_to_epoch(dt):
+    epoch = datetime.utcfromtimestamp(0)
+    delta = dt - epoch
+    return delta.total_seconds()
+
+def query_lastfm(user_name,api_key,page,to_date):
+	""" to_date must be in epoch timestamp format (integer)"""
+	url="http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user="+user_name+"&api_key="+api_key+"&format=json&page="+str(page)+"&to="+str(to_date)
+	print "Loading stuff from url: {}".format(url)
+
+	return urllib2.urlopen(url).read()
+
+def parse_track_info(track):
+	artist = track["artist"]["#text"]
+	song = track["name"]
+	date_str = track["date"]["#text"]
+	date_listened = datetime.strptime(date_str,"%d %b %Y, %H:%M")
+	return {"artist":artist,"song":song,"date_listened":date_listened}
+
 USER_FOLDER = "./users/"
+
+## parse input data ##
 
 if not os.path.exists(USER_FOLDER):
 	os.mkdir(USER_FOLDER)
 user_name = sys.argv[1] 
 api_key = os.environ['LAST_FM_API_KEY']
 save_file = os.path.join(USER_FOLDER,user_name+".csv")
+if os.path.exists(save_file):
+	have_data = True
+else:
+	have_data = False
 
 
 if len(sys.argv)<3:
 
-	page_limit = 5
+	query_limit = 5
 else:
-	page_limit = int(sys.argv[2])
+	query_limit = int(sys.argv[2])
+
+
+## load archived data, if any ##
+
+if have_data == True:
+	songdf_archive = pd.read_csv(save_file, encoding='utf-8')
+	songdf_archive.date_listened = songdf_archive.date_listened.apply(lambda x: datetime.strptime(x,"%Y-%m-%d %H:%M:%S"))
+        songdf_archive = songdf_archive.set_index("idx")
+	max_date = songdf_archive.date_listened.max()
+	min_date = songdf_archive.date_listened.min()
+else:
+	max_date = datetime.datetime.utcfromtimestamp(0)
 
 songs = []
 
-for page in range(1,page_limit+1):
-	url="http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user="+user_name+"&api_key="+api_key+"&format=json&page="+str(page)
-	print "Loading stuff from url: {}".format(url)
+start_date_epoch = datetime_to_epoch(datetime.now())
 
+page = 1
+
+## query last.fm for recent tracks ##
+
+for queries in range(0,query_limit):
+	
 	time.sleep(1)
 
-	text = urllib2.urlopen(url).read()
+	text = query_lastfm(user_name,api_key,page,int(start_date_epoch))
 
 	try:
 		tracks = json.loads(text)["recenttracks"]["track"]
@@ -38,31 +79,38 @@ for page in range(1,page_limit+1):
 		print "Unexpected json format. Either no moar songs or last.fm thinks I am spamming.. will work with what I have"
 		break
 
+	page+=1
 
-	for track in tracks:
+	for track,i in zip(tracks,range(0,len(tracks))):
 	
+		track_info = parse_track_info(track)
 
-		artist = track["artist"]["#text"]
-		song = track["name"]
-		date_str = track["date"]["#text"]
-		date_listened = datetime.strptime(date_str,"%d %b %Y, %H:%M")
-		songs.append({"artist":artist,"song":song,"date_listened":date_listened})
+		if track_info["date_listened"]<=max_date:
+			# if the date listened of the newly fetched track is before the most recent stored track 
+			# start teh backfilling
+			start_date_epoch = datetime_to_epoch(min_date)
+			max_date = datetime.utcfromtimestamp(0)
+			print "Page {}, track {} already in data: Will use remaining queries to query dates before and up to {}".format(page-1,i+1,min_date)
+			page=1			
+			break
+		
+		songs.append(track_info)
 
-if len(songs)==0:
+if len(songs)==0 and not have_data:
 
 	print "No songs for user {}".format(user_name)
 	sys.exit(0)
 
-songdf = pd.DataFrame(songs).drop_duplicates() # if there are two entries with same artist,song and timestamp, only one will be kept
-songdf.index.name = "idx"
 
-if os.path.exists(save_file):
-	songdf_archive = pd.read_csv(save_file, encoding='utf-8')
-	songdf_archive.date_listened = songdf_archive.date_listened.apply(lambda x: datetime.strptime(x,"%Y-%m-%d %H:%M:%S"))
-        songdf_archive = songdf_archive.set_index("idx")
+songdf = pd.DataFrame(songs).drop_duplicates() # if there are two entries with same artist,song and timestamp, only one will be kept
+songdf.index.name = "idx" #could use timestamp as the index...
+
+if have_data == True:
 	songdf = songdf_archive.append(songdf).drop_duplicates()
 
 songdf.to_csv(save_file,encoding='utf-8')
+
+## Do statistics on the data , create views etc ##
 
 #unique tracks
 
@@ -97,10 +145,4 @@ print "Most popular day : {}".format(most_popular_day)
 songdf["cnt"]=1 # helper column to show that each entry counts for one
 #pivot call creates a pandas series, which can be ordered by value (the sum of 1s, ie the count)
 most_popular_day_alt = songdf.pivot_table(rows="day_of_week",  values="cnt", aggfunc=np.sum).order(ascending=False).index[0]
-assert(most_popular_day == most_popular_day_alt)
-
-
-
-
-
-	
+assert(most_popular_day == most_popular_day_alt)	
